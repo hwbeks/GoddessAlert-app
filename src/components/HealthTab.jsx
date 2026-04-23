a49f59aca15362110af6bf25de72d699de9e5d68
Hwbeks <hwbeks@users.noreply.github.com>
2026-04-23 21:46:09 +0200

    Update HealthTab.jsx

--- src/components/HealthTab.jsx
+++ src/components/HealthTab.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { supabase } from "../supabaseClient";
+import { supabase } from "../supabase";
 import { T, css } from "../theme";
 import TheCode from "./TheCode";
 
