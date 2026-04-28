929af309d0f15ef5bf2e1c02b8e092924b9ed6e0
Hwbeks <hwbeks@users.noreply.github.com>
2026-04-28 05:09:11 +0200

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { inject } from "@vercel/analytics";
+inject();
 
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
